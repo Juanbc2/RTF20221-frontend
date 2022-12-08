3f73acc2f0c5feb815d490ee1fc6f9c9c7c664f5
Juan Guillermo Buitrago Calle <64021772+Juanbc2@users.noreply.github.com>
2022-12-08 11:17:50 -0500

    Delete firebase-config.js

--- src/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export default{
-    apiKey: "AIzaSyBBkwkzvkWDXRMo7SCIFOgRlcEUWG0SG7w",
-    authDomain: "lab520221-frontend.firebaseapp.com",
-    projectId: "lab520221-frontend",
-    storageBucket: "lab520221-frontend.appspot.com",
-    messagingSenderId: "727053703100",
-    appId: "1:727053703100:web:9a35b44f51fbabc42e7db9"
-  }
